59c0125329f5866ce4f42d84e366bcc8df064b3a
TakumaOgawa <syuu.2531@gmail.com>
2020-10-11 19:05:53 +0900

    課題5完成

--- Task5/ViewController.swift
+++ Task5/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var answerLabel: UILabel!
+    @IBOutlet private weak var dividendTextField: UITextField!
+    @IBOutlet private weak var divisorTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dividendTextField.keyboardType = .numberPad
+        divisorTextField.keyboardType = .numberPad
+    }
+    
+    @IBAction func calculationButtonTapped(_ sender: Any) {
+        
+        if dividendTextField.text == "" {
+            displayAlert(title: "課題5", message: "割られる数を入力して下さい")
+            return
+        } else if divisorTextField.text == "" {
+            displayAlert(title: "課題5", message: "割る数を入力して下さい")
+            return
+        } else if Double(divisorTextField.text!)! == 0 {
+            displayAlert(title: "課題5", message: "割る数には0を入力しないで下さい")
+            return
+        }
+        
+        let answer = Double(dividendTextField.text!)! / Double(divisorTextField.text!)!
+        answerLabel.text = String(answer)
+        
+    }
+    
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
     }
 
 
 }
-
